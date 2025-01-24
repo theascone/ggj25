e93edbbf155bf63b5e4e65240480a8ef9fef6080
Samenfranz <mallewax118@gmail.com>
2025-01-24 20:00:50 +0100

    added wabering

--- scenes/bubble.gd
+++ scenes/bubble.gd
@@ -3,15 +3,18 @@ extends CharacterBody2D
 
 @export var acceleration = 600.0
 @export var dampening = 0.9
+@export var sine_amplitude = 250.0
+@export var sine_frequency = 0.0035
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
-	velocity += get_gravity() * delta
+	#velocity += get_gravity() * delta
 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	print(direction)
+
 	velocity += direction * acceleration * delta
+	velocity += Vector2(0.0, sin(Time.get_ticks_msec()*sine_frequency))
 	
 	velocity = lerp(velocity, Vector2(0.0, 0.0), dampening * delta)
 	
