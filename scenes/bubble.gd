c59cbcf45a252c4cefdade569deab5953066c829
Addis Dittebrandt <addis.dittebrandt@posteo.net>
2025-01-24 19:42:25 +0100

    basic player character

---
+++ scenes/bubble.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+@export var acceleration = 600.0
+@export var dampening = 0.9
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+	velocity += get_gravity() * delta
+
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	print(direction)
+	velocity += direction * acceleration * delta
+	
+	velocity = lerp(velocity, Vector2(0.0, 0.0), dampening * delta)
+	
+	move_and_slide()
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
