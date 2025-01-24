c922a30d29a8767e2bdf91966ba1e7b57114377c
Addis Dittebrandt <addis.dittebrandt@posteo.net>
2025-01-25 00:19:45 +0100

    camera speed

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+@export var speed = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +10,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var player = get_node("/root/Root/Player").controlled
-	position = lerp(position, player.position, 1 * delta)
+	position = lerp(position, player.position, speed * delta)
