658640cd0a282250876ab7d16f9dc525cc59796d
Samenfranz <mallewax118@gmail.com>
2025-01-25 20:39:20 +0100

    fixed bee again

--- scripts/enemy_stinger.gd
+++ scripts/enemy_stinger.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 @export var SPEED = 300.0
-@export var hit_rate = 0.5
+@export var attack_area = 400.0
 @export var bee_velocity = 100.0
-@export var sting_velocity = 500.0
+@export var sting_velocity = 150.0
+
+var sting_fired = false
 
 func _ready():
 	$AnimatedSprite2D.play("regular_smile")
@@ -15,13 +17,26 @@ func _physics_process(delta: float) -> void:
 	if player.controlled == null:
 		return
 	var dir = (player.controlled.position - position).normalized() 
-	var query = PhysicsRayQueryParameters2D.create(position, dir*200.0)
+	var query = PhysicsRayQueryParameters2D.create(position, dir * attack_area)
 	var result = space_state.intersect_ray(query)
 	if result.size() != 0:
 		if result["collider"] == player.controlled:
+			$AnimatedSprite2D.play("angry")
 			velocity = bee_velocity * dir
-			$StingSprite.rotation = (dir * Vector2(-1, 1)).angle_to(Vector2(0, 1))
-			$StingSprite.dir = dir
-			$StingSprite.sting_speed = 500.0
+			
+			if not sting_fired:
+				#velocity = bee_velocity * dir
+				$StingSprite.rotation = (dir * Vector2(-1, 1)).angle_to(Vector2(0, 1))
+				$StingSprite.dir = dir
+				$StingSprite.sting_speed = sting_velocity
+				sting_fired = true
+		else:
+			$AnimatedSprite2D.play("regular_smile")
+			velocity = Vector2(0.0, 0.0)
 			
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Bubble:
+		body.pop()
